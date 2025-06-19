b06505636f83fc9a1ed1539a053e53b9bbcb6a15
Logidharanir <logidharan0403@gmail.com>
2025-06-19 15:47:00 +0530

    Update App.py

--- App.py
+++ App.py
@@ -27,7 +27,7 @@ def get_gsheet_client():
         "https://www.googleapis.com/auth/spreadsheets",
         "https://www.googleapis.com/auth/drive"
     ]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["gcp_service_account"], scope)
     client = gspread.authorize(creds)
     return client
 
